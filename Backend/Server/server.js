d8755629cfe7a0148358119fe84bb9d3cbd4a5c3
Gouri Ranade <gourianand2003@gmail.com>
2025-11-14 13:10:34 +0530

    Server added

---
+++ Backend/Server/server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const cors= require('cors')
+import Signup from "../pages/Signup"
+// const authorization = require('./routes/authorization')
+// const Review =require('./routes/authorization')
+// app.use('/review',reviewrouter)
+// app.use('/addreview'.addreview)
+app.listen(5000, 'localhost',()=>{
+    console.log('server started at 5000')
+})
